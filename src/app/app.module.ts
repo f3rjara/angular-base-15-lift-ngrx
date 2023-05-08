af9a06de3423e6aeece79722ad908d84ce9097bd
Fernando Jaramillo <f3rjara@gmail.com>
2023-05-08 16:14:23 -0500

    Init Project - Angular 15 - Jest - EsLint - Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+/* Components */
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
