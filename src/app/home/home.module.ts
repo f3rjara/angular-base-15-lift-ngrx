b4b05eae28197549c6689fa7411cf3769fcacadd
Fernando Jaramillo <f3rjara@gmail.com>
2023-05-09 12:31:48 -0500

    Add standalont Component Use

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { AloneButtonComponent } from '../shared/components/atoms/alone-button/alone-button.component';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    AloneButtonComponent
   ]
 })
 export class HomeModule { }
---
+++ src/app/shared/components/atoms/alone-button/alone-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AloneButtonComponent } from './alone-button.component';
+
+describe('AloneButtonComponent', () => {
+  let component: AloneButtonComponent;
+  let fixture: ComponentFixture<AloneButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ AloneButtonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AloneButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
